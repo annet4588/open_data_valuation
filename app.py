470359080fd008c1115d4b3032839b36f73668ee
annet4588 <annet4588@yahoo.co.uk>
2025-10-23 21:47:43 +0100

    Added Value Dimenstion,  Step 1 - Select Dataset, & Step 2 - Select Use Case

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import pandas as pd
 from src.dataset_quality import DatasetQualityValuator
 
 # Streamlit setup
-st.set_page_config(page_title="Open Data Valuation", layout="wide")
+st.set_page_config(page_title="Open Data Valuation Tool", layout="wide")
 st.title("Open Data Valuation Dashboard")
+st.markdown("Use this Tool to assess the value of open datasets based on strategic dimentions.")
 
+# Value Dimentions
+value_dimensions= ["Economic", "Social", "Environmental", "Cultural", "Policy Alignment", "Data Quality"]
+
+# 1. SELECT DATASET
+st.header("1. Select Dataset")
 # File uploader
 uploaded_file = st.file_uploader(
     "Upload a CSV or Excel file", type=["csv", "xlsx", "xls"]
@@ -30,11 +36,25 @@ if uploaded_file:
         # Show Preview
         st.subheader("Data Preview")
         st.dataframe(df.head(), width="stretch")
-        
-        # Evaluate dataset quality 
+
+        # Evaluate dataset quality
+        st.subheader("Data Quality Overview:")
         dq = DatasetQualityValuator(df)
         quality = dq.score()
         st.json(quality)
+        
+        # 2. SELECT USE CASE
+        st.header("2. Select Use Case") 
+        # All Use Cases
+        use_cases = ["Planning & Development", "Policy Monitoring & Reporting","Public Engagement & Awareness", "Regulatory Compliance Monitoring", "Water Quality Risk Assessment"]
+        # User's selected use case
+        selected_use_case = st.selectbox(
+            "Choose a Use Case", 
+            use_cases, 
+            index=None, 
+            placeholder="Select Use Case...")
+        # Show selected use case
+        st.write("Selected Use Case: ", selected_use_case)
         
     except Exception as e:
         st.error(f"Failed to reaf file: {e}")
